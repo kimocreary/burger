75d49efbfeb8708ac27568e7a82e6b9bb9bad2a8
Kimberlyn Creary <kimocreary@gmail.com>
2019-11-14 11:28:09 -0500

    routes added

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burgers = require("../models/burgers.js");
+
+
+
+
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheeseburger', true);
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Veggie Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Delux Burger', true);
